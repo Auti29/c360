ae4bb4b0a49fa3e8427c9fee9f6424ecf3b04a6a
achyutauti30@gmail.com <achyutauti30@gmail.com>
2025-03-05 15:33:28 +0530

    app committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-<<<<<<< HEAD
 import HrHome from "./pages/HrHome";
 import UserAssessment from "./pages/UserAssessment";
 import Assessment from "./pages/Assessment";
-=======
-import HRDashboardPage from "./pages/HrDashboard";
-import './index.css';
-import HRTestCreation from "./pages/HRTestCreation";
->>>>>>> 7d09945b9c630de4a82ac7dc4ca96807299c6255
 import "./index.css";
 
 const App = () => {
@@ -17,15 +11,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
         <Route path="/dashboard" element={<HrHome />} />
         <Route path="/userassessment" element={<UserAssessment />} />
         <Route path="/assessment" element={<Assessment />} />
-=======
-        <Route path="/signup/recruiter" element={<HRDashboardPage />} />
-        <Route path="/createTest" element={<HRTestCreation />} />
-        
->>>>>>> 7d09945b9c630de4a82ac7dc4ca96807299c6255
       </Routes>
     </Router>
   );
